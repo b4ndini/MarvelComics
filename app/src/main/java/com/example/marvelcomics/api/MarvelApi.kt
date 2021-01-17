42e0438a395ea54809cdd112d367ea0ffac13f69
Luis Felipe <lfelipe.maior@gmail.com>
2021-01-17 19:37:19 -0300

    Melhorando a chamada da API e adicionando ScrollView na tela de detalhes da Hq/API call improvement and ScrollView on Comics Detail Activity[2~

--- app/src/main/java/com/example/marvelcomics/api/MarvelApi.kt
+++ app/src/main/java/com/example/marvelcomics/api/MarvelApi.kt
@@ -3,10 +3,15 @@ package com.example.marvelcomics.api
 import com.example.marvelcomics.model.Comics
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 
 interface MarvelApi {
 
     @GET("comics")
-    suspend fun comics() : Response<Comics>
+    suspend fun comics(
+    @Query("limit") limite : Int = 100
+    ): Response<Comics>
+
+
 }
